a31359e267ed690dbd332edea122e20b830abb3c
Gemy77777 <gemysherief@gmail.com>
2025-08-14 18:01:59 +0300

    updated bookmark websites

--- Python/Bookmark websites.py
+++ Python/Bookmark websites.py
@@ -1,9 +1,11 @@
 MyFavouriteList = []
-MaximumBookmarks = 3
+MaximumBookmarks = 5
 while 0 < MaximumBookmarks:
-    MyFavouriteListInput = input('Please enter your favourite website (https://): ')
+    MyFavouriteListInput = input('Please enter your favourite website https://: ')
     MyFavouriteList.append(MyFavouriteListInput)
     MaximumBookmarks -= 1
 else:
     print('You have reached the maximum number of bookmarks.')
-    print(f'Your favourite websites are: {MyFavouriteList.lower()}')
+    print('Your favourite websites are:')
+    for website in MyFavouriteList:
+        print(f"https://{website}")
