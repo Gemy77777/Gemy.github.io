ffc07ceb49140ba6f34ec06a8fa1aa040a5aa82a
Gemy77777 <gemysherief@gmail.com>
2025-08-16 14:33:15 +0300

    added a file

--- Python/function training.py
+++ Python/function training.py
@@ -1,5 +1,17 @@
-def say_hello(*names):
-    for name in names:
-        print(f"Hello, {name}!")
+MySkills = {
+    "Python": "90%",
+    "JavaScript": "60%",
+    "HTML": "80%",
+    "CSS": "70%"
+}
+MyTuple = ('go', 'c++', 'figma')
 
-say_hello('Alice', 'Bob', 'Charlie', 'Gemy')
+def display_tools(tools):
+    for tool in tools:
+        print(f"lang: {tool}")
+def display_skills(skills):
+    for skill, level in skills.items():
+        print(f"{skill}: {level}")
+display_skills(MySkills)
+print(50 * "=")
+display_tools(MyTuple)
