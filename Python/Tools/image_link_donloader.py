fd6a2be3c5fe4279660fcedeb4b1ce9695db4c1f
Gemy77777 <gemysherief@gmail.com>
2026-07-12 13:09:02 +0200

    made WevDev folder

--- Python/Tools/image_link_donloader.py
+++ Python/Tools/image_link_donloader.py
@@ -1,5 +1,5 @@
 import requests
-r = requests.get("https://images.unsplash.com/photo-1511300636408-a63a89df3482?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjJ8fGRlc2t0b3AlMjB3YWxscGFwZXJ8ZW58MHx8MHx8fDA%3D")
+r = requests.get("https://www.nvisia.com/insights/agile-methodology/images/agile-methodology.jpg")
 
 with open("images/image.jpg", "wb") as f:
     f.write(r.content)
